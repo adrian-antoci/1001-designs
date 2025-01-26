0f6c76c941688dcc3befbd085bb8fb602535c708
Adrian Antoci <antociadrian91@gmail.com>
2025-01-26 14:57:43 +0000

    add database.dat file

---
+++ generate_directory_list.sh
@@ -0,0 +1,10 @@
+search_dir=.
+output_file_path=./database.dat
+rm -rf $output_file_path
+
+for entry in */
+do
+  if [[ "$entry" != "_platform/" ]]; then
+    echo ${entry:0:$((${#entry} - 1))} >> $output_file_path
+  fi
+done
